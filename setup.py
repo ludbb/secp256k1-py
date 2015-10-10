0e63c717e00d6d9d7cfc3142c8d3d2d85a9c8b5f
Ludvig Broberg <lud@tutanota.com>
2015-10-10 19:38:51 +0000

    no need to distribute the build.py module

--- setup.py
+++ setup.py
@@ -12,14 +12,14 @@ classifiers = [
 
 setup(
     name="secp256k1",
-    version="0.8.1",
+    version="0.8.2",
     description='FFI bindings to secp256k1',
     author='Ludvig Broberg',
     author_email='lud@tutanota.com',
     url='https://github.com/ludbb/secp256k1-py',
     license='MIT',
     classifiers=classifiers,
-    py_modules=['secp256k1', 'build'],
+    py_modules=['secp256k1'],
     setup_requires=['cffi>=1.0.0', 'pytest-runner'],
     cffi_modules=['build.py:ffi'],
     install_requires=['cffi>=1.0.0'],
